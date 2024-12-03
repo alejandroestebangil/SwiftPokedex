21575b0746af22d6b506797f66b9c1dcbd91405d
Esteban, Alejandro1 <Alejandro.Esteban2@adidas.com>
2024-12-03 12:56:21 +0100

    Create Base Project

---
+++ AdidasPokedex/Sources/AdidasPokedexApp.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct AdidasPokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Project.swift
@@ -0,0 +1,34 @@
+import ProjectDescription
+
+let project = Project(
+    name: "AdidasPokedex",
+    targets: [
+        .target(
+            name: "AdidasPokedex",
+            destinations: .iOS,
+            product: .app,
+            bundleId: "io.tuist.AdidasPokedex",
+            infoPlist: .extendingDefault(
+                with: [
+                    "UILaunchScreen": [
+                        "UIColorName": "",
+                        "UIImageName": "",
+                    ],
+                ]
+            ),
+            sources: ["AdidasPokedex/Sources/**"],
+            resources: ["AdidasPokedex/Resources/**"],
+            dependencies: []
+        ),
+        .target(
+            name: "AdidasPokedexTests",
+            destinations: .iOS,
+            product: .unitTests,
+            bundleId: "io.tuist.AdidasPokedexTests",
+            infoPlist: .default,
+            sources: ["AdidasPokedex/Tests/**"],
+            resources: [],
+            dependencies: [.target(name: "AdidasPokedex")]
+        ),
+    ]
+)
