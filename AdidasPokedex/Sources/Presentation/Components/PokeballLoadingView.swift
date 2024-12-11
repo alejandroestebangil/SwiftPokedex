444170c9b6c431aa059d5296cbd60097effbafa1
Esteban, Alejandro <Alejandro.Esteban2@adidas.com>
2024-12-11 15:40:02 +0100

    Loading View

--- AdidasPokedex/Sources/Presentation/Components/PokeballLoadingView.swift
+++ AdidasPokedex/Sources/Presentation/Components/PokeballLoadingView.swift
@@ -1,8 +1,32 @@
 //
-//  File.swift
+//  PokeballLoadingView.swift
 //  AdidasPokedex
 //
-//  Created by Esteban, Alejandro on 11/12/24.
+//  Created by Esteban, Alejandro on 10/12/24.
 //
 
-import Foundation
+import SwiftUI
+
+struct PokeballLoadingView: View {
+    @State private var isRotating = false
+    
+    var body: some View {
+        VStack {
+            Image("pokeball-logo")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 300, height: 300)
+                .rotationEffect(.degrees(isRotating ? 360 : 0))
+                .animation(
+                    .linear(duration: 1)
+                    .repeatForever(autoreverses: false),
+                    value: isRotating
+                )
+                .onAppear {
+                    isRotating = true
+                }
+        }
+    }
+}
+
+
